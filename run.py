91614004b230380afd7416d2f503f29e3fe56514
JJNod88 <j.noddings@hotmail.com>
2022-01-27 19:19:31 +0000

    Added an intro function. Captures name and rules

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 def main_quiz():
     """
-    This will be the functionality of my main game
-    Quizzes can test their knowledge of a particular genre
+    This is where my Quiz will start.
     """
-    pass
+    user_name = input("Hey, please enter your name:")
+
+    while user_name == "" or user_name == " ":
+        print('Oops, please enter your name')
+        user_name = input("Hey, please enter your name:")
+    else: 
+        print(f'Hey {user_name}, Lets test your knowledge of the world')
+        print('We have a range of questions for you to go at')
+        print("All you have to do is select 'a', 'b', 'c', or 'd'")
+        print('You got thios. Good Luck)')
+
+
+
 
 def answers():
     """
@@ -12,33 +23,34 @@ def answers():
     pass
 
 
-# dictionary of questions the game will be asking, 
+# dictionary of questions the game will be asking,
+
 quiz_questions = [
-    {"Question": "I am on top of the largest human built structure in the world. Where am I?: .",
+    {"Question": "Location of the tallest human built structure in the world.\nWhere am I?: ",
     "Answers": {"a": "Japan",
                 "b": "Hong Kong",
                 "c": "Dubai",
                 "d": "Abu Dhabi"},
     "correct Answer": "c"},
-    {"Question": "A group of Pyramids whos name begins with the letter 'G', are in which Country?",
+    {"Question": "A group of Pyramids beginning with the letter 'G'.\nWhich Country are they in?: ",
     "Answers": {"a": "Mexico",
                 "b": "placeholder",
                 "c": "placeholder",
                 "d": "Egypt"},
     "correct Answer": "d"},
-    {"Question": "I am on the set of Game of Thrones. Which country am I in?: .",
+    {"Question": "I am on the set of Game of Thrones. Which country am I in?: ",
     "Answers": {"a": "New Zealand",
                 "b": "Ireland",
                 "c": "Scotland",
                 "d": "Denmark"},
     "correct Answer": "b"},
-    {"Question": "I am north of Italy, South of Sweden. My Capital City begins with a B. What City am i in?: ",
+    {"Question": "I am north of Italy, South of Sweden. My Capital City begins with a B.\nWhat City am i in?: ",
     "Answers": {"a": "Poland",
                 "b": "Germany",
                 "c": "Bulgaria",
                 "d": "France"},
     "correct Answer": "b"},
-    {"Question": "I am in the USA. My state is famously known for my Canyon. What City am I in?: ",
+    {"Question": "I am in the USA. My state is famously known for my Canyon.\nWhat City am I in?: ",
     "Answers": {"a": "Arizona",
                 "b": "Texas",
                 "c": "California",
@@ -50,7 +62,7 @@ quiz_questions = [
                 "c": "Switzerland",
                 "d": "Sweden"},
     "correct Answer": "c"},
-    {"Question": "If I was sailing between the UK and USA, what Sea would i likely be in?",
+    {"Question": "If I was sailing between the UK and USA, what Sea am i in?: ",
     "Answers": {"a": "Atlantic.",
                 "b": "Pacific.",
                 "c": "Indian.",
