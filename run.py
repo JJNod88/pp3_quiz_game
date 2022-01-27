916f610ab059faabae6b0edd576ab627f8187fca
JJNod88 <j.noddings@hotmail.com>
2022-01-27 18:21:44 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main_quiz():
+    """
+    This will be the fucntionality of my main game
+    """
+    pass
+
+def answers():
+    """
+    I will include my answers here and link to my main game
+    """
+    pass
+
+
+
