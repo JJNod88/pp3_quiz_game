959c6079496ada7b03b24dca4a06f6499bff4715
JJNod88 <j.noddings@hotmail.com>
2022-01-29 09:42:11 +0000

    Added print statement for the question before the answers

--- run.py
+++ run.py
@@ -24,6 +24,7 @@ def main_quiz(quiz_questions):
     for i in quiz_questions:
         answer = ''
         while answer not in ['a', 'b', 'c', 'd']:
+            print(i['question'])
             for key, value in i['choices'].items():
                 print(f' {key} : {value}')
 
@@ -34,50 +35,54 @@ def main_quiz(quiz_questions):
 
         if answer == i['correct_choice']:
             print('Correct! Well done.\n')
+        
 
         else:
             print('Oh no, that was incorrect.\n')
             
 # dictionary of questions the game will be asking
 
+
+
+
 quiz_questions = [
-    {"Question": "Location of the tallest human built structure in the world.\nWhere am I?: ",
+    {"question": "Location of the tallest human built structure in the world.\nWhere am I?: ",
     "choices": {"a": "Japan",
                 "b": "Hong Kong",
                 "c": "Dubai",
                 "d": "Abu Dhabi"},
     "correct_choice": "c"},
-    {"Question": "A group of Pyramids beginning with the letter 'G'.\nWhich Country are they in?: ",
+    {"question": "A group of Pyramids beginning with the letter 'G'.\nWhich Country are they in?: ",
     "choices": {"a": "Mexico",
                 "b": "placeholder",
                 "c": "placeholder",
                 "d": "Egypt"},
     "correct_choice": "d"},
-    {"Question": "I am on the set of Game of Thrones. Which country am I in?: ",
+    {"question": "I am on the set of Game of Thrones. Which country am I in?: ",
     "choices": {"a": "New Zealand",
                 "b": "Ireland",
                 "c": "Scotland",
                 "d": "Denmark"},
     "correct_choice": "b"},
-    {"Question": "I am north of Italy, South of Sweden. My Capital City begins with a B.\nWhat City am i in?: ",
+    {"question": "I am north of Italy, South of Sweden. My Capital City begins with a B.\nWhat City am i in?: ",
     "choices": {"a": "Poland",
                 "b": "Germany",
                 "c": "Bulgaria",
                 "d": "France"},
     "correct_choice": "b"},
-    {"Question": "I am in the USA. My state is famously known for my Canyon.\nWhat City am I in?: ",
+    {"question": "I am in the USA. My state is famously known for my Canyon.\nWhat City am I in?: ",
     "choices": {"a": "Arizona",
                 "b": "Texas",
                 "c": "California",
                 "d": "Florida"},
     "correct_choice": "a"},
-    {"Question": "'Fondue' is famously recognised for which Country?: '",
+    {"question": "'Fondue' is famously recognised for which Country?: '",
     "choices": {"a": "Belgium",
                 "b": "France",
                 "c": "Switzerland",
                 "d": "Sweden"},
     "correct_choice": "c"},
-    {"Question": "If I was sailing between the UK and USA, what Sea am i in?: ",
+    {"question": "If I was sailing between the UK and USA, what Sea am i in?: ",
     "choices": {"a": "Atlantic.",
                 "b": "Pacific.",
                 "c": "Indian.",
